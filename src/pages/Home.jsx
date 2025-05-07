3e38dbc365982f822a06c4072905e55b9ff32a33
Greycode009 <gamerdipesh002@gmail.com>
2025-05-07 23:02:18 +0545

    fix logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,7 +82,7 @@ function Home() {
           >
             {/* Using logo.png as Dipesh Malla's image */}
             <img
-              src="https://i.postimg.cc/j5SB8L1m/logo.png"
+              src="https://i.ibb.co/prB36m9q/logo.png"
               alt="Dipesh Malla"
               className="w-full h-auto block"
             />
