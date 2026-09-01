919d08621e5e142604472f57650dccc52fd56710
Dipesh Malla <gamerdipesh002@gmail.com>
2026-09-01 14:24:59 +0545

    chore: remove Android platform and Capacitor dependencies

--- android/app/src/main/java/com/dipesh/portfolio/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.dipesh.portfolio;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dipesh.portfolio',
-  appName: 'Dipesh Port',
-  webDir: 'build'
-};
-
-export default config;
